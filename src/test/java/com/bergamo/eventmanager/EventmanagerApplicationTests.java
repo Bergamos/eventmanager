c4d13f6f89e219813d2a6a04ed7b15b269bd657a
Alisson Bergamin <alisson.bergs@gmail.com>
2025-05-28 19:13:06 -0300

    Project creation

---
+++ src/test/java/com/bergamo/eventmanager/EventmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bergamo.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
